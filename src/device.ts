509483f42ca5c157935c17eb1344f3e69ad432c9
dblue <dblue@deloitte.com>
2021-01-04 23:11:39 -0500

    pull data directly from device

--- src/device.ts
+++ src/device.ts
@@ -1,23 +1,79 @@
-import { devices, HID } from 'node-hid';
-import { Observable } from 'rxjs';
+import { EventEmitter } from 'events';
+import { HID } from 'node-hid';
 
-const VENDOR_ID = 9408;
-const PRODUCT_ID = 3;
+type ReportKey = [number, number];
 
-export function getStation() {
-  const device = new HID(
-    '\\\\?\\hid#vid_24c0&pid_0003#6&362ca017&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}'
-  );
+const REPORT_1: ReportKey = [0x0100 + 1, 10];
+// const REPORT_2: ReportKey = [0x0100 + 2, 25];
 
-  console.log(device.getFeatureReport(0, 100));
+export class Device extends EventEmitter {
+  private r1NextRead = 0;
+  private device = new HID(this.vendorId, this.productId);
 
-  return new Observable((subscriber) => {
-    device.on('data', (res) => {
-      subscriber.next(res);
-    });
+  constructor(private vendorId: number, private productId: number) {
+    super();
 
-    device.on('error', (err) => {
-      subscriber.error(err);
-    });
-  });
+    this.startListening();
+  }
+
+  private startListening() {
+    if (Date.now() >= this.r1NextRead) {
+      const report = this.device.getFeatureReport(...REPORT_1);
+      const flavor = this.decodeMessageFlavor(report);
+      const timestamp = new Date();
+
+      const fields: any = {
+        windSpeed: this.round(this.decodeWindSpeed(report), 0),
+      };
+
+      if (flavor === 1) {
+        fields.rain = this.round(this.decodeRain(report), 2);
+      } else {
+        fields.outTemp = this.round(this.decodeOutTemp(report), 0);
+        fields.outHumid = this.decodeOutHumid(report);
+      }
+
+      this.r1NextRead = Date.now() + 18 * 1000;
+
+      this.emit('data', { measurement: 'weather', timestamp, fields });
+    }
+
+    setTimeout(() => this.startListening(), 18 * 1000);
+  }
+
+  private decodeMessageFlavor(data: number[]) {
+    return data[3] & 0x0f;
+  }
+
+  private decodeWindSpeed(data: number[]) {
+    const n = ((data[4] & 0x1f) << 3) | ((data[5] & 0x70) >> 4);
+
+    if (n == 0) {
+      return 0.0;
+    }
+
+    return (0.8278 * n + 1.0) / 1.609;
+  }
+
+  private decodeRain(data: number[]) {
+    const cm = (((data[6] & 0x3f) << 7) | (data[7] & 0x7f)) * 0.0254;
+
+    return cm / 2.54;
+  }
+
+  private decodeOutTemp(data: number[]) {
+    const a = (data[5] & 0x0f) << 7;
+    const b = data[6] & 0x7f;
+    const celcius = (a | b) / 18.0 - 40.0;
+
+    return (celcius * 9) / 5 + 32;
+  }
+
+  private decodeOutHumid(data: number[]) {
+    return data[7];
+  }
+
+  private round(value: number, decimals: number) {
+    return Number(Math.round(Number(value + 'e' + decimals)) + 'e-' + decimals);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,95 +1,21 @@
-import { deepEqual } from 'assert';
-import { HID } from 'node-hid';
-
-type ReportKey = [number, number];
-
-interface Report {
-  type: 1 | 8;
-}
-
-interface RainReport {}
+import { createDbConnection } from './db';
+import { Device } from './device';
 
 const VENDOR_ID = 0x24c0;
 const PRODUCT_ID = 0x0003;
-const REPORT_1: ReportKey = [0x0100 + 1, 10];
-const REPORT_2: ReportKey = [0x0100 + 2, 25];
 
-const R1_INTERVAL = 18 * 1000;
+console.log('App Starting...');
 
-const device = new HID(VENDOR_ID, PRODUCT_ID);
-let r1NextRead = 0;
+const { INFLUXDB_HOST = 'localhost' } = process.env;
 
-pollDevice();
+const influx = createDbConnection({
+  host: INFLUXDB_HOST,
+});
 
-function pollDevice() {
-  if (Date.now() >= r1NextRead) {
-    const report = device.getFeatureReport(...REPORT_1);
-    const flavor = decodeMessageFlavor(report);
+const device = new Device(VENDOR_ID, PRODUCT_ID);
 
-    let data: any = {
-      windSpeed: round(decodeWindSpeed(report), 0),
-    };
+device.on('data', async (data) => {
+  await influx.writePoints([data]);
 
-    switch (flavor) {
-      case 1: {
-        data.rain = round(decodeRain(report), 2);
-
-        break;
-      }
-
-      case 8: {
-        data.outTemp = round(decodeOutTemp(report), 0);
-        data.outHumid = decodeOutHumid(report);
-
-        break;
-      }
-    }
-
-    r1NextRead = Date.now() + R1_INTERVAL;
-
-    console.log('Message Flavor:', flavor);
-    console.table(data);
-  }
-
-  const delay = r1NextRead - Date.now() + 1;
-
-  console.log(`next read in ${delay / 1000} seconds`);
-
-  setTimeout(pollDevice, delay);
-}
-
-function decodeRain(data: number[]) {
-  const cm = (((data[6] & 0x3f) << 7) | (data[7] & 0x7f)) * 0.0254;
-
-  return cm / 2.54;
-}
-
-function decodeOutTemp(data: number[]) {
-  const a = (data[5] & 0x0f) << 7;
-  const b = data[6] & 0x7f;
-  const celcius = (a | b) / 18.0 - 40.0;
-
-  return (celcius * 9) / 5 + 32;
-}
-
-function decodeOutHumid(data: number[]) {
-  return data[7];
-}
-
-function decodeMessageFlavor(data: number[]) {
-  return data[3] & 0x0f;
-}
-
-function decodeWindSpeed(data: number[]) {
-  const n = ((data[4] & 0x1f) << 3) | ((data[5] & 0x70) >> 4);
-
-  if (n == 0) {
-    return 0.0;
-  }
-
-  return (0.8278 * n + 1.0) / 1.609;
-}
-
-function round(value: number, decimals: number) {
-  return Number(Math.round(Number(value + 'e' + decimals)) + 'e-' + decimals);
-}
+  console.log(data);
+});
