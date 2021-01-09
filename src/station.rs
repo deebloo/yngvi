fd6b9f292249efb9c48bca5979639f948f2cef78
dblue <dblue@deloitte.com>
2021-01-09 00:57:09 -0500

    cleanup

--- src/station.rs
+++ src/station.rs
@@ -114,7 +114,6 @@ impl Station<'_> {
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
--- src/util.rs
+++ src/util.rs
@@ -3,15 +3,14 @@ pub fn calc_wind_chill(wind_speed: f32, out_temp: f32) -> f32 {
         return out_temp as f32;
     }
 
-    let raw = 35.74 + 0.6215 * out_temp as f32 - 35.75 * wind_speed.powf(0.16)
-        + 0.4275 * out_temp as f32 * wind_speed.powf(0.16);
+    let speed = wind_speed.powf(0.16);
+    let raw = 35.74 + 0.6215 * out_temp - 35.75 * speed + 0.4275 * out_temp * speed;
 
     raw
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
