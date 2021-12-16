aa27c63caa8fef4b5c2bc9c5382b9afcbb590b8f
dblue <dblue@deloitte.com>
2021-12-16 10:17:14 -0500

    remove test writer

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,6 @@ mod influx;
 use acurite_core::config;
 use serde::Deserialize;
 
-struct TestWriter;
-
-#[async_trait::async_trait]
-impl acurite_core::Writer for TestWriter {
-    async fn write(&mut self, weather_reading: &acurite_core::WeatherReading) -> Result<(), ()> {
-        println!("{}", weather_reading);
-
-        Ok(())
-    }
-}
-
 #[derive(Deserialize, Debug, Clone)]
 pub enum Station {
     CONSOLE,
