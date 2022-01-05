4d130c6504d1549b1200f6719d62b38abb65ecab
dblue <dblue@deloitte.com>
2022-01-05 10:13:29 -0500

    update stuff

--- tests/rewrite_test.rs
+++ tests/rewrite_test.rs
@@ -36,11 +36,11 @@ async fn should_replay_failed_writes_rtl_433() {
     let mut reader = test_reader::RTL433TestReader {
         current_reading: 0,
         readings: vec![
-            String::from("{\"time\" : \"2021-12-15T20:48:18Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 2, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 3.193, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}"),
-            String::from("{\"time\" : \"2021-12-15T20:48:37Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 49, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 2, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 2.679, \"wind_dir_deg\" : 90.000, \"rain_in\" : 41.830, \"mic\" : \"CHECKSUM\"}"),
-            String::from("{\"time\" : \"2021-12-15T20:48:56Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 2, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 4.222, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}"),
-            String::from("{\"time\" : \"2021-12-15T20:49:14Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 49, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 2, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 4.736, \"wind_dir_deg\" : 180.000, \"rain_in\" : 41.830, \"mic\" : \"CHECKSUM\"}"),
-            String::from("{\"time\" : \"2021-12-15T20:49:33Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 2, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 2.679, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}")
+            String::from("{\"time\" : \"2021-12-15T20:48:18Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 3.193, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}"),
+            String::from("{\"time\" : \"2021-12-15T20:48:37Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 49, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 2.679, \"wind_dir_deg\" : 90.000, \"rain_in\" : 41.830, \"mic\" : \"CHECKSUM\"}"),
+            String::from("{\"time\" : \"2021-12-15T20:48:56Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 4.222, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}"),
+            String::from("{\"time\" : \"2021-12-15T20:49:14Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 49, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 4.736, \"wind_dir_deg\" : 180.000, \"rain_in\" : 41.830, \"mic\" : \"CHECKSUM\"}"),
+            String::from("{\"time\" : \"2021-12-15T20:49:33Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 2.679, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}")
         ],
     };
 
