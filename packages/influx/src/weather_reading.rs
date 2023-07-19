3898065ab4c288c454ccf538199573aa4dc032eb
deebloo <dannybluedesign@gmail.com>
2023-07-19 16:17:13 -0400

    fix influx tests

--- packages/influx/src/weather_reading.rs
+++ packages/influx/src/weather_reading.rs
@@ -51,7 +51,7 @@ mod tests {
             rain_delta: Some(0.1),
             wind_speed: Some(1.0),
             wind_dir: Some(270.0),
-            wind_dir_cardinal: None,
+            wind_dir_cardinal: Some("SE"),
             out_temp: Some(80.0),
             out_humid: Some(50),
             wind_chill: Some(70.0),
@@ -67,7 +67,7 @@ mod tests {
                 rain_delta: Some(0.1),
                 wind_speed: Some(1.0),
                 wind_dir: Some(270.0),
-                wind_dir_cardinal: Some("hello")
+                wind_dir_cardinal: Some("SE"),
                 out_temp: Some(80.0),
                 out_humid: Some(50),
                 wind_chill: Some(70.0),
