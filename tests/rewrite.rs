f2dee32c321ed94d54cea0363ebcd0db6af1029f
deebloodd <dblue@deloitte.com>
2023-08-31 17:05:34 -0400

    more test data

--- tests/rewrite.rs
+++ tests/rewrite.rs
@@ -7,7 +7,7 @@ mod error_writer;
 async fn should_replay_failed_writes_rtl_433() {
     let mut station = ws_core::Station::new();
 
-    let source = rtl_433_file_source("tests/data/rtl_433_data_1.txt");
+    let source = rtl_433_file_source("data/rtl_433.txt").take(5);
     let reader = RTL433Reader::new(source);
 
     let mut writer = error_writer::ErrorWriter {};
@@ -16,9 +16,8 @@ async fn should_replay_failed_writes_rtl_433() {
 
     assert_eq!(station.retry_manager.failed_writes.len(), 5);
 
-    let reader = RTL433Reader::new(vec![
-        Ok(String::from("{\"time\" : \"2021-12-15T20:48:18Z\", \"model\" : \"Acurite-5n1\", \"message_type\" : 56, \"id\" : 1306, \"channel\" : \"A\", \"sequence_num\" : 0, \"battery_ok\" : 1, \"wind_avg_mi_h\" : 3.193, \"temperature_F\" : 55.800, \"humidity\" : 70, \"mic\" : \"CHECKSUM\"}")),
-    ].into_iter());
+    let source = rtl_433_file_source("data/rtl_433.txt").take(5);
+    let reader = RTL433Reader::new(source.take(1));
 
     let mut writer = InMemWriter::new();
 
