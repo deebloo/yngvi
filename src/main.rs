4d43f02f56c2dd33e2f10e6c612cf68967beab72
deebloodd <dblue@deloitte.com>
2023-08-31 13:01:09 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,48 @@ use rtl_433::{rtl_433_source, RTL433Reader};
 use std::env;
 use weather::{InMemWriter, Station, StdoutWriter, WeatherReading};
 
+enum AppWriter {
+    Influx(InfluxWriter),
+    Stdout(StdoutWriter),
+    InMemory(InMemWriter),
+}
+
 #[tokio::main]
 async fn main() {
     let source: String = env::var("WEATHER_SOURCE").unwrap_or("CONSOLE".to_string());
     let dest: String = env::var("WEATHER_DEST").unwrap_or("INFLUXDB".to_string());
 
     let mut station = Station::new();
-
-    let reader: Box<dyn Iterator<Item = WeatherReading>> = match source.to_uppercase().as_str() {
-        "CONSOLE" => Box::new(DisplayReader::new(
-            HidSource::new(0x24c0, 0x003).expect("could not start HID Api"),
-        )),
-        "RTL433" => Box::new(RTL433Reader::new(rtl_433_source())),
-        _ => panic!("no reader defined"),
-    };
+    let reader = find_reader(source.to_uppercase().as_str());
+    let mut writer = find_writer(dest.to_uppercase().as_str());
 
     println!(
         "Starting weather program. Reading from {} and writing to {}",
         source, dest
     );
 
-    match dest.to_uppercase().as_str() {
-        "INFLUXDB" => station.start(reader, &mut InfluxWriter::new()).await,
-        "INMEMORY" => station.start(reader, &mut InMemWriter::new()).await,
-        "STDOUT" => station.start(reader, &mut StdoutWriter::new()).await,
+    match &mut writer {
+        AppWriter::Influx(writer) => station.start(reader, writer).await,
+        AppWriter::InMemory(writer) => station.start(reader, writer).await,
+        AppWriter::Stdout(writer) => station.start(reader, writer).await,
+    }
+}
+
+fn find_reader(value: &str) -> Box<dyn Iterator<Item = WeatherReading>> {
+    match value {
+        "CONSOLE" => Box::new(DisplayReader::new(
+            HidSource::new(0x24c0, 0x003).expect("could not start HID Api"),
+        )),
+        "RTL433" => Box::new(RTL433Reader::new(rtl_433_source())),
+        _ => panic!("no reader defined"),
+    }
+}
+
+fn find_writer(value: &str) -> AppWriter {
+    match value {
+        "INFLUXDB" => AppWriter::Influx(InfluxWriter::new()),
+        "INMEMORY" => AppWriter::InMemory(InMemWriter::new()),
+        "STDOUT" => AppWriter::Stdout(StdoutWriter::new()),
         _ => panic!("no writer defined"),
-    };
+    }
 }
