6dcf321a9d0d46c714372a4da99aabba6a68305b
deebloo <dannybluedesign@gmail.com>
2023-09-04 21:01:20 -0400

    move line protocol tests

--- packages/influx_db/src/line_protocol.rs
+++ packages/influx_db/src/line_protocol.rs
@@ -59,3 +59,40 @@ impl LineProtocol for WeatherReading {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_line_protocol_0() {
+        let reading = WeatherReading {
+            device_id: Some(100),
+            time: chrono::Utc::now(),
+            rain: Some(100.),
+            rain_delta: Some(0.5),
+            wind_speed: Some(4.),
+            wind_dir: Some(180.),
+            wind_dir_cardinal: Some("S".to_string()),
+            out_temp: Some(60.5),
+            out_humid: Some(50),
+            wind_chill: Some(50.),
+            heat_index: Some(60.),
+            dew_point: Some(90.),
+        };
+
+        assert_eq!(reading.to_line_protocol(), format!("weather device_id=100i,rain=100,rain_delta=0.5,wind_speed=4,wind_dir=180,wind_dir_cardinal=\"S\",out_temp=60.5,out_humid=50i,wind_chill=50,heat_index=60,dew_point=90 {}", reading.time.timestamp_nanos()));
+    }
+
+    #[test]
+    fn should_handle_empty_values() {
+        let mut reading = WeatherReading::new();
+
+        reading.out_temp = Some(60.);
+
+        assert_eq!(
+            reading.to_line_protocol(),
+            format!("weather out_temp=60 {}", reading.time.timestamp_nanos())
+        );
+    }
+}
